60436ea4f71acfd71265016eee28a641aa898fab
vinay badhan <vinay.badhan21@gmail.com>
2017-04-01 02:44:11 +0530

    Add files via upload

---
+++ pdf_access/tocpdf.php
@@ -0,0 +1,13 @@
+<?php
+$query="SELECT `users`.`id`, `pdffile`.`subject_id`, `users`.`firstname`, `pdffile`.`pdflocation`,`pdffile`.`pdftopic` FROM `users` LEFT JOIN `pdffile` ON `users`.`id`=`pdffile`.`people_id`";
+if($query_run=mysqli_query($conn,$query)){
+    while($row=mysqli_fetch_assoc($query_run)){
+        if($row['subject_id']=='5'){
+            echo '<a href="'.$row["pdflocation"].'" style="color:black; padding:0px 0px 0px 15px;font-size:18px">'.$row["pdftopic"].' [<span style="color:blue">Download</span>]<br></a>';
+        }
+    }
+}
+else{
+    echo "No data to display.";
+}
+?>
